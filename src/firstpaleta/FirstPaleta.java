4fc1639a9e28e5ad6ad0eca0e858e519db661e4e
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-04-10 12:00:07 +0200

    1ª paleta

---
+++ src/firstpaleta/FirstPaleta.java
@@ -0,0 +1,14 @@
+
+
+package firstpaleta;
+
+
+public class FirstPaleta {
+
+   
+    public static void main(String[] args) {
+       Ventana obx=new Ventana();
+       obx.setVisible(true);
+    }
+    
+}
